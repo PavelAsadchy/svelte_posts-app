9f932ccbc5deefaeccd651cb4c83b99bdbfd048c
Pavel Asadchy <asadchy.pavel@gmail.com>
2022-10-18 12:01:16 +0200

    style: transitions

--- src/lib/FeedbackList.svelte
+++ src/lib/FeedbackList.svelte
@@ -1,12 +1,15 @@
 <script>
+  import { fade, scale } from 'svelte/transition';
   import FeedbackItem from './FeedbackItem.svelte';
   
   export let feedbackList;
 </script>
 
 {#each feedbackList as feedbackItem (feedbackItem.id)}
-  <FeedbackItem
-    on:delete-feedback
-    item={feedbackItem}
-  />
+  <div in:scale out:fade="{{ duration: 500 }}">
+    <FeedbackItem
+      on:delete-feedback
+      item={feedbackItem}
+    />
+  </div>
 {/each}
